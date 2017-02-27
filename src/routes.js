450a91668089275f1d890b772eb496b17ae51717
Basil Khan <basilkhan.dev@gmail.com>
2017-02-27 00:38:59 -0500

    refactor(configureStore): React-Router, conf store, Provider, Root

--- src/routes.js
+++ src/routes.js
@@ -3,32 +3,9 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import Home from './views/Home';
 import Product from './views/Product';
-import { localState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore';
 
-import { combineReducers, createStore } from 'redux';  
-import { Provider } from 'react-redux';
-
-import allProducts from './reducers';
-
-const persistedState = {
-	products: [{
-		id: '0',
-		product: 'Sample Beer',
-		is_available: false,
-	}],
-};
-
-const store = createStore(
-	allProducts,
-	persistedState
-); 
-
-store.subscribe(throttle(() => {
-	saveState(store.getState());
-}, 1000));
-
-console.log(store.getState());
+const store = configureStore();
 
 export default (
   <Route path="/" component={App}>
